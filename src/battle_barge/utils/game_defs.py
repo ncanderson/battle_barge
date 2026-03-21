ba720f6e89e8e3a0eec0ca7c26db21376593fe54
Nate Anderson <nanderson@greensea.com>
2026-03-21 13:16:02 -0400

    add game defs

---
+++ src/battle_barge/utils/game_defs.py
@@ -0,0 +1,15 @@
+# Standard imports
+from enum import Enum, auto
+
+# 3rd party imports
+
+# Module imports
+
+################################################################################
+
+class Difficulty(Enum):
+    EASY = auto()
+    MEDIUM = auto()
+    HARD = auto()
+
+################################################################################
